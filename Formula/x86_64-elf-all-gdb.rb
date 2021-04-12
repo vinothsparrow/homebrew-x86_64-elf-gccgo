40440dfb9d4d27627214b98ec54873bdce22044a
Vinoth Kumar <3402812+vinothsparrow@users.noreply.github.com>
2021-04-12 18:17:42 +0530

    Create x86_64-elf-all-gdb.rb

---
+++ Formula/x86_64-elf-all-gdb.rb
@@ -0,0 +1,39 @@
+class X8664ElfAllGdb < Formula
+  desc "GNU debugger for i386-elf cross development"
+  homepage "https://www.gnu.org/software/gdb/"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-10.1.tar.xz"
+  sha256 "f82f1eceeec14a3afa2de8d9b0d3c91d5a3820e23e0a01bbb70ef9f0276b62c0"
+  license "GPL-3.0-or-later"
+  head "https://sourceware.org/git/binutils-gdb.git"
+
+  depends_on "python@3.9"
+  depends_on "xz"
+
+  conflicts_with "gdb", because: "both install include/gdb, share/gdb and share/info"
+  conflicts_with "i386-elf-gdb", because: "both install include/gdb, share/gdb and share/info"
+
+  def install
+    args = %W[
+      --target=x86_64-elf
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --with-lzma
+      --enable-targets=all
+      --with-python=#{Formula["python@3.9"].opt_bin}/python3
+      --disable-binutils
+    ]
+
+    mkdir "build" do
+      system "../configure", *args
+      system "make"
+
+      system "make", "install-gdb"
+    end
+  end
+
+  test do
+    system "#{bin}/x86_64-elf-gdb", "#{bin}/x86_64-elf-gdb", "-configuration"
+  end
+end
