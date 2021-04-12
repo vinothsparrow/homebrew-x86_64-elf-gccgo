fd143aaa5e0c6151f060c7a52d82d0f4c99d84a1
Vinoth Kumar <3402812+vinothsparrow@users.noreply.github.com>
2021-04-12 12:46:36 +0530

    Create x86_64-elf-gccgo.rb

---
+++ Formula/x86_64-elf-gccgo.rb
@@ -0,0 +1,47 @@
+class X8664ElfGccGo < Formula
+  desc "GNU compiler collection for x86_64-elf"
+  homepage "https://gcc.gnu.org"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-10.3.0/gcc-10.3.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-10.3.0/gcc-10.3.0.tar.xz"
+  sha256 "64f404c1a650f27fc33da242e1f2df54952e3963a49e06e73f6940f3223ac344"
+
+  depends_on "gmp"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "x86_64-elf-binutils"
+
+  def install
+    mkdir "x86_64-elf-gcc-build" do
+      system "../configure", "--target=x86_64-elf",
+                             "--prefix=#{prefix}",
+                             "--infodir=#{info}/x86_64-elf-gcc",
+                             "--disable-nls",
+                             "--without-isl",
+                             "--without-headers",
+                             "--with-as=#{Formula["x86_64-elf-binutils"].bin}/x86_64-elf-as",
+                             "--with-ld=#{Formula["x86_64-elf-binutils"].bin}/x86_64-elf-ld",
+                             "--enable-languages=c,c++,go"
+      system "make", "all-gcc"
+      system "make", "install-gcc"
+      system "make", "all-target-libgcc"
+      system "make", "install-target-libgcc"
+
+      # FSF-related man pages may conflict with native gcc
+      (share/"man/man7").rmtree
+    end
+  end
+
+  test do
+    (testpath/"test-c.c").write <<~EOS
+      int main(void)
+      {
+        int i=0;
+        while(i<10) i++;
+        return i;
+      }
+    EOS
+    system "#{bin}/x86_64-elf-gcc", "-c", "-o", "test-c.o", "test-c.c"
+    assert_match "file format elf64-x86-64",
+      shell_output("#{Formula["x86_64-elf-binutils"].bin}/x86_64-elf-objdump -a test-c.o")
+  end
+end
